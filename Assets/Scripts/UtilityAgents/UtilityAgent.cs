0186873d7d6addf1db0c8085290e767bf31ce599
JimmyStephen <jstephen@student.neumont.edu>
2022-02-25 11:51:55 -0700

    2/25

--- Assets/Scripts/UtilityAgents/UtilityAgent.cs
+++ Assets/Scripts/UtilityAgents/UtilityAgent.cs
@@ -1,11 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class UtilityAgent : Agent
 {
     [SerializeField] MeterUI meter;
+    [SerializeField] Perception perception;
+
+    const float MIN_SCORE = 0.1f;
+
     Need[] needs;
+    UtilityObject activeUtilityObject = null;
+    public bool isUsingUtilityObject { get { return activeUtilityObject != null; } }
 
     public float happiness
     {
@@ -24,13 +31,32 @@ public class UtilityAgent : Agent
     {
         needs = GetComponentsInChildren<Need>();
         meter.text.text = "";
+
     }
 
     private void Update()
     {
         animator.SetFloat("Speed", movement.velocity.magnitude);
-        meter.slider.value = 1 - happiness;
-        meter.worldPosition = transform.position;
+        if(activeUtilityObject == null)
+        {
+            var gameObjects = perception.GetGameObjects();
+            List<UtilityObject> utilityObjects = new List<UtilityObject>();
+            foreach(var go in gameObjects)
+            {
+                if(go.TryGetComponent<UtilityObject>(out UtilityObject utilityObject))
+                {
+                    utilityObject.visible = true;
+                    utilityObject.score = GetUtilityObjectScore(utilityObject);
+                    if(utilityObject.score > MIN_SCORE) utilityObjects.Add(utilityObject);
+                }
+            }
+
+            activeUtilityObject = (utilityObjects.Count == 0) ? null : utilityObjects[0];
+            if(activeUtilityObject != null)
+            {
+                StartCoroutine(ExecuteUtilityObject(activeUtilityObject));
+            }
+        }
     }
 
     private void OnGUI()
@@ -45,5 +71,77 @@ public class UtilityAgent : Agent
             offset += 20;
         }
         //GUI.Label(new Rect(screen.x + 20, Screen.height - screen.y - offset, 300, 20), mood.ToString());
+    }
+
+    private void LateUpdate()
+    {
+        meter.slider.value = 1 - happiness;
+        meter.worldPosition = transform.position;
+    }
+
+    IEnumerator ExecuteUtilityObject(UtilityObject utilityObject)
+    {
+        //go to location
+        movement.MoveTowards(utilityObject.location.position);
+        while(Vector3.Distance(transform.position, utilityObject.location.position) > 0.5)
+        {
+            Debug.DrawLine(transform.position, utilityObject.location.position);
+            yield return null;
+        }
+
+        //start effect
+        print("Start Effect");
+        if (utilityObject.effect != null) utilityObject.effect.SetActive(true);
+
+        //wait duration
+        yield return new WaitForSeconds(utilityObject.duration);
+
+        //end effect
+        print("End Effect");
+        if (utilityObject.effect != null) utilityObject.effect.SetActive(false);
+
+        //Apply
+        applyUtilityObject(utilityObject);
+        
+        //set active to null
+        activeUtilityObject = null;
+
+        //exit
+        yield return null;
+    }
+
+    void applyUtilityObject(UtilityObject utilityObject)
+    {
+        foreach (var effector in utilityObject.effectors)
+        {
+            Need need = GetNeedByType(effector.type);
+            if (need != null)
+            {
+                need.input += effector.change;
+                need.input = Mathf.Clamp(need.input, -1, 1);
+            }
+        }
+    }
+
+    float GetUtilityObjectScore(UtilityObject utilityObject)
+    {
+        float score = 0;
+
+        foreach(var effector in utilityObject.effectors)
+        {
+            Need need = GetNeedByType(effector.type);
+            if(need != null)
+            {
+                float futureNeed = need.getMotive(need.input + effector.change);
+                score += need.motive - futureNeed;
+            }
+        }
+
+        return score;
+    }
+
+    Need GetNeedByType(Need.Type type)
+    {
+        return needs.First(need => need.type == type);
     }
 }
--- Assets/Scripts/UtilityAgents/UtilityObject.cs
+++ Assets/Scripts/UtilityAgents/UtilityObject.cs
@@ -9,17 +9,27 @@ public class UtilityObject : MonoBehaviour
     {
         public Need.Type type;
         [Range(-2, 2)] public float change;
-
     }
+
+    [SerializeField] MeterUI meterPrefab;
 
     public float duration;
     public Transform location;
+    public GameObject effect;
 
     public Effector[] effectors;
     public Dictionary<Need.Type, float> registry = new Dictionary<Need.Type, float>();
 
+    public float score { get; set; }
+    public bool visible { get; set; }
+    MeterUI meter;
+
     void Start()
     {
+        meter = Instantiate(meterPrefab, GameObject.Find("Canvas").transform);
+        meter.name = name;
+        meter.text.text = name;
+
         foreach(var effector in effectors)
         {
             registry[effector.type] = effector.change;
@@ -35,5 +45,13 @@ public class UtilityObject : MonoBehaviour
     public bool HasEffector(Need.Type type)
     {
         return registry.ContainsKey(type);
+    }
+
+    private void LateUpdate()
+    {
+        meter.gameObject.SetActive(visible);
+        meter.worldPosition = transform.position + (Vector3.up * 3);
+        meter.slider.value = score;
+        visible = false;
     }
 }
